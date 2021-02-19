b27525962397055d6aea5d2a29b919cf514d5f32
Francesco Anastasio <f.anastasio@avanade.com>
2021-02-19 09:18:05 +0100

    Minor update.

--- ImportIntoAzureCosmosDb/Program.cs
+++ ImportIntoAzureCosmosDb/Program.cs
@@ -65,8 +65,8 @@ namespace ImportIntoAzureCosmosDb
             Microsoft.Azure.Cosmos.CosmosClient client = new Microsoft.Azure.Cosmos.CosmosClient(account, key);
 
             CosmosDbService cosmosDbService = new CosmosDbService(client, databaseName, containerName);
-            Microsoft.Azure.Cosmos.DatabaseResponse database = await client.CreateDatabaseIfNotExistsAsync(databaseName);
-            await database.Database.CreateContainerIfNotExistsAsync(containerName, configurationSection.GetSection("PartitionKey").Value);
+            //Microsoft.Azure.Cosmos.DatabaseResponse database = await client.CreateDatabaseIfNotExistsAsync(databaseName);
+            //await database.Database.CreateContainerIfNotExistsAsync(containerName, configurationSection.GetSection("PartitionKey").Value);
 
 
             /**
